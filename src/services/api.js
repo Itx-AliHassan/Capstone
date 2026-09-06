5042252fbf48f387c4ecb507871d8bd733012bad
Ali Hassan <itx.alihassanch@gmail.com>
2026-09-06 17:31:07 +0500

    final project3 with backend

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000';
 
 const api = axios.create({
-  baseURL: `${backendUrl}`,
+  baseURL: `${backendUrl}/api`,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
